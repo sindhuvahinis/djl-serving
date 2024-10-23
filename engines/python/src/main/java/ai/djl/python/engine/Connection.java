9407c214380a8dcaa11b084e46b063b75ee3d8ac
Somasundaram <somasundaram.sindhu@gmail.com>
2024-10-23 16:44:57 -0700

    slot

--- engines/python/src/main/java/ai/djl/python/engine/Connection.java
+++ engines/python/src/main/java/ai/djl/python/engine/Connection.java
@@ -224,7 +224,7 @@ class Connection {
             args[4] = "--bind-to";
             args[5] = "none";
             args[6] = "--map-by";
-            args[7] = "pe-list=0-95";
+            args[7] = "slot:pe-list=0-95";
             args[8] = "--mca";
             args[9] = "btl_vader_single_copy_mechanism";
             args[10] = "none";
